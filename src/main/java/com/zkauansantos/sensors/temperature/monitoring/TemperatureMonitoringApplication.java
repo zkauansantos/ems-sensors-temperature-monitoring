012661d1977c8e564327858ab428c847450ad4ba
Kauan Santos <kauansantosdepontes@gmail.com>
2026-09-18 15:17:16 -0300

    chore: init all submodules

---
+++ src/main/java/com/zkauansantos/sensors/temperature/monitoring/TemperatureMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.zkauansantos.sensors.temperature.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureMonitoringApplication.class, args);
+	}
+
+}
