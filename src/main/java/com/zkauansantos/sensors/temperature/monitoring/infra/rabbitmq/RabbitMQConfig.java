d78854787986a26557b53dc20fee3957cf8b2da3
Kauan Santos <kauansantosdepontes@gmail.com>
2026-09-23 15:23:16 -0300

    feat: create exchanges and queues on rabbitmq

---
+++ src/main/java/com/zkauansantos/sensors/temperature/monitoring/infra/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,29 @@
+package com.zkauansantos.sensors.temperature.monitoring.infra.rabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public Queue queue(){
+        return QueueBuilder.durable("temperature-monitoring.process-temperature.v1.q").build();
+    }
+
+    public FanoutExchange exchange(){
+        return ExchangeBuilder.fanoutExchange("temperature-processing.temperature-received.v1.e").build();
+    }
+
+    @Bean
+    public Binding binding(){
+        return BindingBuilder.bind(queue()).to(exchange());
+    }
+}
